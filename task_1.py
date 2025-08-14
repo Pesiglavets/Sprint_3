af8c791d9617ee0b8d98452306ae949e730578eb
Pesiglavets <russdebut8@gmail.com>
2025-08-14 12:54:26 +0300

    added second method

--- task_1.py
+++ task_1.py
@@ -15,3 +15,11 @@ class OnlineSalesRegisterCollector:
     @property
     def number_items(self):   
         return self.__number_items    
+    
+    def add_item_to_cheque(self, name):
+        if not name or len(name) > 40:
+            raise ValueError('Нельзя добавить товар, если в его названии нет символов или их больше 40')
+        if name not in self.__item_price:
+            raise NameError('Позиция отсутствует в товарном справочнике')
+        self.__name_items.append(name)
+        self.__number_items += 1
