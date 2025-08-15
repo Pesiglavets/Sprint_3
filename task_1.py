408983852ebdc44c038f0d8711acfa9f18dcac2f
Pesiglavets <russdebut8@gmail.com>
2025-08-15 10:38:44 +0300

    added 8th method

--- task_1.py
+++ task_1.py
@@ -67,3 +67,11 @@ class OnlineSalesRegisterCollector:
     
     def total_tax(self):
         return self.twenty_percent_tax_calculation() + self.ten_percent_tax_calculation()
+    
+    @staticmethod
+    def get_telephone_number(telephone_number):
+        if type(telephone_number) is not int:
+            raise ValueError('Необходимо ввести цифры')
+        if len(str(telephone_number)) > 10:
+            raise ValueError('Необходимо ввести 10 цифр после "+7"')
+        return f"+7{telephone_number}"
