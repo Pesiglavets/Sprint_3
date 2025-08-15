1f54827b00d2ffa24c2232a49f51222ee91812e5
Pesiglavets <russdebut8@gmail.com>
2025-08-15 10:53:30 +0300

    added 9th method

--- task_1.py
+++ task_1.py
@@ -75,3 +75,12 @@ class OnlineSalesRegisterCollector:
         if len(str(telephone_number)) > 10:
             raise ValueError('Необходимо ввести 10 цифр после "+7"')
         return f"+7{telephone_number}"
+    
+    @staticmethod
+    def get_date_and_time():
+        date_and_time = []
+        now = datetime.datetime.now()
+        date = [['часы', lambda x: x.hour], ['минуты', lambda x: x.minute], ['день', lambda x: x.day], ['месяц', lambda x: x.month], ['год', lambda x: x.year]]
+        for item in date:
+            date_and_time.append(f'{item[0]}: {item[1](now)}')
+        return date_and_time
